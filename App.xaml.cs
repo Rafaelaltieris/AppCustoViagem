74de3caf32c640b083e64cd7d5617b0ef5e05839
Rafael <65348541+Rafaelaltieris@users.noreply.github.com>
2022-10-04 11:40:48 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AppCustoViagem.Model;
+using AppCustoViagem.View;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+using System.Globalization;
+using System.Threading;
+
+namespace AppCustoViagem
+{
+    public partial class App : Application
+    {
+        public static List<Pedagio> ListaPedagios = new List<Pedagio>();
+
+        public App()
+        {
+            InitializeComponent();
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            MainPage = new NavigationPage(new Listagem());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
